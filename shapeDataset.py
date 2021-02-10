f71abffb4bdf38c7b2621641dc01f1cdc4ed39be
Gerson <gerson.foks@gmail.com>
2021-02-10 14:17:46 +0100

    made the shape game dataset

--- shapeDataset.py
+++ shapeDataset.py
@@ -1,3 +1,5 @@
+from itertools import permutations, product
+
 from torch.utils.data import Dataset, DataLoader
 from torchvision.datasets import MNIST, FashionMNIST, CIFAR10
 import numpy as np
@@ -50,3 +52,64 @@ class ShapeDataset(Dataset):
             item = self.transform(item)
 
         return item, self.targets[idx]
+
+
+class ShapeGameDataset(Dataset):
+    '''
+    The dataset for a simple mnist signlalling game.
+    Each image gets n_receiver-1 other images to be compared with.
+    '''
+
+    def __init__(self, epoch_size=10e4, n_receiver=3, picture_size=32, shape_size=8, transform=None):
+        self.epoch_size = epoch_size
+
+        self.n_receiver = n_receiver
+
+        self.picture_size = picture_size
+        self.shape_size = shape_size
+        self.possible_coordinates = [i * shape_size for i in range(int(picture_size / shape_size))]
+
+        self.sender_items, self.receiver_items, self.targets = self.generate_items()
+        self.transform = transform
+
+    def generate_items(self):
+        # First generate the pairs we want
+
+        sender_items = []
+        receiver_items = []
+        targets = []
+
+        possible_items = list(product(COLORS, SHAPES))
+        for i in range(self.n_receiver):
+            target_index = np.random.choice(self.n_receiver, 1)[0]
+            item_ids = np.random.choice(len(possible_items), self.n_receiver, replace=False)
+            x_coordinates = np.random.choice(self.possible_coordinates, self.n_receiver)
+            y_coordinates = np.random.choice(self.possible_coordinates, self.n_receiver)
+            items = [
+                make_img_one_shape(x, y, possible_items[id][0], possible_items[id][1], size=self.shape_size,
+                                   picture_size=self.picture_size) for
+                x, y, id in
+                zip(x_coordinates, y_coordinates, item_ids)
+            ]
+
+            sender_items.append(items[target_index])
+            receiver_items.append(items)
+            targets.append(target_index)
+
+        return sender_items, receiver_items, targets
+
+    def __len__(self):
+        return len(self.epoch_size)
+
+    def __getitem__(self, idx):
+        sender_item = self.sender_items[idx]
+        receiver_item = self.receiver_items[idx]
+        if self.transform:
+            sender_item = self.transform(sender_item)
+            receiver_item = [
+                self.transform(item) for item in receiver_item
+            ]
+
+        return sender_item, receiver_item, self.targets[idx]
+
+
--- shape_dataset_debug.py
+++ shape_dataset_debug.py
@@ -1,7 +1,16 @@
 from torchvision.transforms import transforms
 
-from shapeDataset import ShapeDataset
+from shapeDataset import ShapeDataset, ShapeGameDataset
 
 transform = transforms.Compose([transforms.ToTensor()])
 shape_dataset = ShapeDataset(epoch_size=10, picture_size=64, shape_size=16, transform=transform)
 
+transform = transforms.Compose([transforms.ToTensor()])
+shape_game = ShapeGameDataset(epoch_size=10, picture_size=64, shape_size=16,)
+
+for (sender_item, receiver_items, target) in shape_game:
+    sender_item.show()
+    for item in receiver_items:
+        item.show()
+    print(target)
+    break
